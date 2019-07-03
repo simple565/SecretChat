5b8516cbb002727d84ef6d226dd0fa50f5f2d58c
Maureen <467914885@qq.com>
2019-07-03 22:35:00 +0800

    添加注释

--- app/src/main/java/com/maureen/secretchat/MainActivity.java
+++ app/src/main/java/com/maureen/secretchat/MainActivity.java
@@ -2,7 +2,11 @@ package com.maureen.secretchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/**
+ * @Description
+ * @Author Lianml
+ * @DATE 2019-07-03 22:32
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
